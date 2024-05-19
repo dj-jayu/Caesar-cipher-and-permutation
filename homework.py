9aff544a4266a1e115d7f12e464cb4464d5ee993
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2024-05-19 02:47:22 -0300

    implemented permutation function

--- homework.py
+++ homework.py
@@ -21,11 +21,41 @@ def caesar_encription(s, key=0, dec=False):
 result = caesar_encription('If you can meet with triumph and disaster, and treat those two impostors just the same.',key=7)
 
 #result = caesar_encription('a',key=1)
-print(f'caesar encription {result}')
+#print(f'caesar encription {result}')
 
 result = caesar_encription('Lt wdas iwtht igjiwh id qt htau-tkxstci, iwpi paa btc pgt rgtpits tfjpa, iwpi iwtn pgt tcsdlts qn iwtxg Rgtpidg lxiw rtgipxc jcpaxtcpqat Gxvwih, iwpi pbdcv iwtht pgt Axut, Axqtgin pcs iwt ejghjxi du Wpeexcthh.', key=15, dec=True)
 #result = caesar_encription('b', key=1, dec=True)
 
-print(f'caesar decription: {result}')
+#print(f'caesar decription: {result}')
 
+def permut(s, l, rule, dec=False):
+    if dec:
+        rule = invert_rule(rule)
+    final_text = []
+    for i in range(0, len(s), l):
+        block = s[i:i+l]
+        block = rearrange(block, l, rule)
+        final_text.append(block)
+    return ''.join(final_text)
 
+def rearrange(block, l, rule):
+    final_block = []
+    for number in rule:
+        final_block.append(block[number-1])
+    return ''.join(final_block)
+
+def invert_rule(permutation):
+    inverse = [0] * len(permutation)
+    
+    for i, x in enumerate(permutation):
+        inverse[x - 1] = i + 1
+    
+    return inverse
+
+result = permut('Dulce et decorum est pro patria mori', 6, (5,4,6,1,3,2))
+print(f'permuted:{result}')
+
+result = permut('Iofy un awt  otste ehret ueem aesru ao f ,mna cwtahw oh th erseta  hsiirnefi,osr  ntohei sqsula', 5, (1,3,5,4,2), dec=False)
+print(result)
+
+print(permut(permut('abcde', 5, (3,2,5,1,4), dec=True), 5, (3,2,5,1,4)))
